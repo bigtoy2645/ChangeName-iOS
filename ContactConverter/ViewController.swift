a254dfb12fa9dae1f3293ae46a39c9147be90aa1
yurim <bigtoy2645@naver.com>
2020-12-02 19:47:11 +0900

    Initial commit

---
+++ ContactConverter/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ContactConverter
+//
+//  Created by yurim on 2020/12/02.
+//
+
+import UIKit
+import Contacts
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /* 변환 버튼 클릭 */
+    @IBAction func convertButtonDidTab(_ sender: UIButton) {
+//        let store = CNContactStore()
+//        
+//        // Update the home email address for John Appleseed
+//        guard let mutableContact = contact.mutableCopy() as? CNMutableContact else { return }
+//        let newEmail = CNLabeledValue(label: CNLabelHome, value: "john@example.com" as NSString)
+//        mutableContact.emailAddresses.append(newEmail)
+//
+//        let saveRequest = CNSaveRequest()
+//        saveRequest.update(mutableContact)
+//        do {
+//            try store.execute(saveRequest)
+//        } catch {
+//            print("Saving contact failed, error: \(error)")
+//            // Handle the error
+//        }
+    }
+    
+
+}
+
