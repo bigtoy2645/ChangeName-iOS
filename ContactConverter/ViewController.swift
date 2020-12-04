d014f2d1da1823c7eb16c0e0bbe7e72b810bf29f
yurim <bigtoy2645@naver.com>
2020-12-04 23:46:49 +0900

    feat: Implement a function to change the contact name

--- ContactConverter/ViewController.swift
+++ ContactConverter/ViewController.swift
@@ -9,30 +9,43 @@ import UIKit
 import Contacts
 
 class ViewController: UIViewController {
+    
+    let contactStore = CNContactStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     /* 변환 버튼 클릭 */
     @IBAction func convertButtonDidTab(_ sender: UIButton) {
-//        let store = CNContactStore()
-//        
-//        // Update the home email address for John Appleseed
-//        guard let mutableContact = contact.mutableCopy() as? CNMutableContact else { return }
-//        let newEmail = CNLabeledValue(label: CNLabelHome, value: "john@example.com" as NSString)
-//        mutableContact.emailAddresses.append(newEmail)
-//
-//        let saveRequest = CNSaveRequest()
-//        saveRequest.update(mutableContact)
-//        do {
-//            try store.execute(saveRequest)
-//        } catch {
-//            print("Saving contact failed, error: \(error)")
-//            // Handle the error
-//        }
+        
+        let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
+        let request = CNContactFetchRequest(keysToFetch: keys)
+
+        do {
+            try self.contactStore.enumerateContacts(with: request, usingBlock: convertContactName(contact:stop:))
+        } catch {
+            print("unable to fetch contacts")
+        }
     }
     
+    /* 성 이름 -> _ 성이름 */
+    func convertContactName(contact: CNContact, stop: UnsafeMutablePointer<ObjCBool>) {
+        
+        if contact.familyName != "" {
+            guard let mutableContact = contact.mutableCopy() as? CNMutableContact else { return }
+            mutableContact.givenName = "\(contact.familyName)\(mutableContact.givenName)"
+            mutableContact.familyName = ""
+            
+            let saveRequest = CNSaveRequest()
+            saveRequest.update(mutableContact)
+            do {
+                try self.contactStore.execute(saveRequest)
+            } catch {
+                print("Saving contact failed, error: \(error)")
+            }
+        }
+    }
 
 }
 
