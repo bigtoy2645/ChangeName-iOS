2ca14aed7ceb9f4fdb3231774df27a5050bfb493
yurim <bigtoy2645@naver.com>
2020-12-05 23:43:39 +0900

    feat: add progress bar

--- ContactConverter/ViewController.swift
+++ ContactConverter/ViewController.swift
@@ -10,28 +10,57 @@ import Contacts
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     let contactStore = CNContactStore()
+    var contacts = [CNContact]()
+    var contactNumber = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        progressBar.progress = 0
     }
     
     /* 변환 버튼 클릭 */
     @IBAction func convertButtonDidTab(_ sender: UIButton) {
+        // 초기화
+        self.progressBar.progress = 0
+        self.contactNumber = 0
+        
+        // 비동기로 수행
+        DispatchQueue.global().async {
+            // 연락처 불러오기
+            self.loadContacts()
+            
+            // 이름 변경
+            for contact in self.contacts {
+                self.convertContactName(contact)
+            }
+        }
+    }
+    
+    /* 연락처 불러오기 */
+    func loadContacts() {
+        // 초기화
+        contacts.removeAll()
         
         let keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName)]
         let request = CNContactFetchRequest(keysToFetch: keys)
-
+        
         do {
-            try self.contactStore.enumerateContacts(with: request, usingBlock: convertContactName(contact:stop:))
+            try self.contactStore.enumerateContacts(with: request, usingBlock: { (contact, stop) in
+                self.contacts.append(contact)
+            })
         } catch {
             print("unable to fetch contacts")
         }
     }
     
     /* 성 이름 -> _ 성이름 */
-    func convertContactName(contact: CNContact, stop: UnsafeMutablePointer<ObjCBool>) {
+    func convertContactName(_ contact: CNContact) {
         
+        // 성이 있는 연락처만 작업
         if contact.familyName != "" {
             guard let mutableContact = contact.mutableCopy() as? CNMutableContact else { return }
             mutableContact.givenName = "\(contact.familyName)\(mutableContact.givenName)"
@@ -44,6 +73,13 @@ class ViewController: UIViewController {
             } catch {
                 print("Saving contact failed, error: \(error)")
             }
+        }
+        
+        // Progress Bar 업데이트
+        DispatchQueue.main.async {
+            let progressStatus = Float(self.contactNumber + 1) / Float(self.contacts.count)
+            self.progressBar.setProgress(progressStatus, animated: true)
+            self.contactNumber += 1
         }
     }
 
